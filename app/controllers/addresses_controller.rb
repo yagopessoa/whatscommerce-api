9117c4da3d6cad845c83cfa9909184d16f5feb89
Yago <yagorp15@gmail.com>
2020-05-14 06:30:39 -0300

    feat(address): created addresses CRUD endpoints

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,70 @@
+class AddressesController < ApplicationController
+  def create
+    users_company = get_users_company
+
+    if users_company != nil
+
+      @address = Address.where(company: users_company).last
+
+      if @address != nil
+        update_existing_address
+      else
+          @address = Address.create(
+            cep: allowed_params['cep'],
+            street: allowed_params['street'],
+            number: allowed_params['number'],
+            neighborhood: allowed_params['neighborhood'],
+            complement: allowed_params['complement'],
+            company: users_company,
+          )
+      end
+
+      render json: @address
+    else
+      head 404
+    end
+  end
+
+  def show
+    users_company = get_users_company
+
+    @address = Address.where(company: users_company).last
+
+    if @address != nil
+      render json: @address
+    else
+      head 404
+    end
+  end
+
+  def update
+    @address = Address.where(company: get_users_company).last
+
+    if @address != nil
+      update_existing_address
+      render json: @address
+    else
+      head 404
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.permit(:cep, :street, :number, :neighborhood, :complement)
+  end
+
+  def get_users_company
+    Company.where(owner: current_user).last
+  end
+
+  def update_existing_address
+    @address.update(
+      cep: allowed_params['cep'] || @address.cep,
+      street: allowed_params['street'] || @address.street,
+      number: allowed_params['number'] || @address.number,
+      neighborhood: allowed_params['neighborhood'] || @address.neighborhood,
+      complement: allowed_params['complement'] || @address.complement,
+    )
+  end
+end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,19 +1,22 @@
 class CompaniesController < ApplicationController
   def create
     if user_signed_in?
-      # TODO: check if company already exists for this user (max: 1)
-      #       if it does, update instead of creating
+      @company = Company.where(owner: current_user).last
 
-      company = Company.create(
-        name: allowed_params['name'],
-        instagram_url: allowed_params['instagram_url'],
-        facebook_url: allowed_params['facebook_url'],
-        whatsapp_number: allowed_params['whatsapp_number'],
-        page_url: allowed_params['page_url'],
-        owner: current_user,
-      )
+      if @company != nil
+        update_existing_company
+      else
+          @company = Company.create(
+            name: allowed_params['name'],
+            instagram_url: allowed_params['instagram_url'],
+            facebook_url: allowed_params['facebook_url'],
+            whatsapp_number: allowed_params['whatsapp_number'],
+            page_url: allowed_params['page_url'],
+            owner: current_user,
+          )
+      end
 
-      head :ok
+      render json: @company
     else
       head :unauthorized
     end
@@ -21,12 +24,32 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.where(owner: current_user).last
-    render json: @company
+
+    if @company != nil
+      render json: @company
+    else
+      head 404
+    end
   end
 
   def update
     @company = Company.where(owner: current_user).last
 
+    if @company != nil
+      update_existing_company
+      render json: @company
+    else
+      head 404
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.permit(:name, :instagram_url, :facebook_url, :whatsapp_number, :page_url)
+  end
+
+  def update_existing_company
     @company.update(
       name: allowed_params['name'] || @company.name,
       instagram_url: allowed_params['instagram_url'] || @company.instagram_url,
@@ -34,13 +57,5 @@ class CompaniesController < ApplicationController
       whatsapp_number: allowed_params['whatsapp_number'] || @company.whatsapp_number,
       page_url: allowed_params['page_url'] || @company.page_url,
     )
-
-    render json: @company
-  end
-
-  private
-
-  def allowed_params
-    params.permit(:name, :instagram_url, :facebook_url, :whatsapp_number, :page_url)
   end
 end
